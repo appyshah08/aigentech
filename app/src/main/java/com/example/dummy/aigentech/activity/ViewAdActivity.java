b043b138b737f4cd366748df23f0c1e581ec21fd
Dhaval <suraj.vishwakarma@ext.icicilombard.com>
2019-10-31 19:20:44 +0530

    third commit

---
+++ app/src/main/java/com/example/dummy/aigentech/activity/ViewAdActivity.java
@@ -0,0 +1,13 @@
+package com.example.dummy.aigentech.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewAdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_ad);
+    }
+}
